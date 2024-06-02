f99dfe2f757d980cb315b9279a79efcfa9347029
Leonardo Ibarra <laibarra.23@est.ucab.edu.ve>
2024-06-02 11:41:57 -0400

    Revert "eeeeee"
    This reverts commit 14d2225950347765c42a59dbe7f94f2d3146eaaa.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
         System.out.println("Prueba commit");
-        System.out.println("Si");
     }
 }
